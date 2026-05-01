0ad336e19c8bbd2d0683a4798b156dea2026b463
NonatoDEv <anonatoavalos@byupathway.edu>
2026-05-01 01:51:05 -0500

    fix: update error messages and comments for clarity in app initialization

--- scripts/app-controler.js
+++ scripts/app-controler.js
@@ -4,29 +4,24 @@ import { Hero } from '/data/hero.js';
 async function initApp() {
     try {
         const response = await fetch('./data/info.json');
-        if (!response.ok) throw new Error('No se pudo cargar el archivo JSON');
-        
+        if (!response.ok) throw new Error('it can not load JSON');
+        //header
         const data = await response.json();
-
-        // 1. Instanciar e Iniciar el Header
-        // Pasamos el nombre corto para el logo
         const header = new Header(data.personalInfo.shortName);
-        header.init(); // Usamos init() para que active render + eventos
-
-        // 2. Instanciar e Iniciar el Hero
+        header.init();
+        //hero
         const heroBanner = new Hero(
-            'containerHero', // El ID del contenedor en el HTML
+            'containerHero',
             data.personalInfo.fullName,
             data.personalInfo.roles
         );
-        heroBanner.render(); // El Hero suele ser más estático, render basta
+        heroBanner.render();
 
-        console.log("Sistema artesanal funcionando.");
+        console.log("System is working.");
 
     } catch (error) {
-        console.error('Error crítico en el inicio de la app:', error);
+        console.error('Critical error initializing the app:', error);
     }
 }
 
-// El punto de partida oficial
 document.addEventListener('DOMContentLoaded', initApp());
